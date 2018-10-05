3caab04d4b20970e9e9d56a91018308c28fcdb0c
Albert Xavier López <39172561+albertxavierlopez@users.noreply.github.com>
2018-10-05 12:38:37 +0200

    Create exercise2.R

---
+++ exercise2.R
@@ -0,0 +1,40 @@
+## EXERCISE 2 #####################################################################
+
+# 2.1. Create a vector X.
+
+x<-c(3,4,1,1,2,1,4,2,1,1,5,3,1,1,1,2,4,5,5,3)
+
+x==1       # elements equal to 1=TRUE
+sum(x==1)  # total 
+
+x[x==1]<-0  #replacing 1 by 0 on vector X
+
+# 2.2. Create a vector Y containing the elements of X that are greater than 1.
+
+y<-x[x>1]
+
+# 2.3. Create a sequence of numbers from 1 to 20 in steps of 0.2 and store.
+
+seq<-seq(1,20, by=0.2)
+seq
+
+# 2.4. Concatenate x and y into a vector called NewVec.
+
+NewVec<-c(x,y)
+
+# 2.5. Display all objects in the workspace and then remove NewVec
+
+ls()
+rm(NewVec)
+
+# 2.6. Create a factor fpain to represent the above data.
+
+patient<-seq(1,6, by=1)
+painlevel<-c(0,3,1,2,1,2)
+
+fpain<-factor(painlevel, levels=0:3)
+levels(fpain)<-c("No pain","Mild pain","Medium pain","Severe pain")
+
+summary(painlevel)
+summary(fpain)
+
